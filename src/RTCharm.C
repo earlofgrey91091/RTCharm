82ff4ea58d62377cce820bd973fc5ded32f60d49
earlofgrey91091 <earlofgrey91091@gmail.com>
2012-11-03 02:44:35 -0500

    many fixes not live viz

--- src/RTCharm.C
+++ src/RTCharm.C
@@ -2,17 +2,24 @@
 #include "shape.h"
 #include "pup_stl.h"
 #include <stdio.h>
+using namespace std;
 #include "RTCharm.decl.h"
+
+
+
 #define ITERATIONS 1000
+#define loc(x,y, width)  (x + (y*width))
+
 
 /*readonly*/ CProxy_Main mainProxy;
 /*readonly*/ int Total_iterations;
 /*readonly*/ int chareDimension;
 class Main : public CBase_Main {
     public:
+        Main_SDAG_CODE;
         CProxy_PixelChare pixel;
-        CkOpts myOpts
-    void run();
+        CkArrayOptions myOpts;
+        
         Main(CkArgMsg* arg) 
         {
             __sdag_init();
@@ -33,10 +40,8 @@ class Main : public CBase_Main {
                 pixel_w = image_w/8;
                 pixel_h = image_h/8;
 
-                CkPrintf("\n [l]: cells will be %d pixels in height," +
-                            "\n cells will be %d pixels in width,\n " + 
-                            "there will be %d cells\n", image_h, image_w, 
-                            chareDimension * chareDimesion);
+                CkPrintf("\n [l]: cells will be %d pixels in height,\ncells will be %d pixels in width,\nthere will be %d cells\n", 
+                            pixel_h, pixel_w, chareDimension * chareDimension);
             }
             else
             {
@@ -52,39 +57,40 @@ class Main : public CBase_Main {
             //Create the image pixel chares based on image size
             pixel = CProxy_PixelChare::ckNew(pixel_w, pixel_h, opts);
             startVis();
-            run();
+            mainProxy.run();
 
         };
+        
+        void startVis()
+        {
+	        int funcIndex = CkIndex_PixelChare::liveVizDump(NULL);
+	        CkCallback cb0(funcIndex, pixel);
 
-    void startVis()
-    {
-	    int funcIndex = CkIndex_Skittish::liveVizDump(NULL);
-	    CkCallback cb0(funcIndex, skitArray);
+	        // Create a liveViz configuration object
+	        liveVizConfig lvConfig(liveVizConfig::pix_color, true);
 
-	    // Create a liveViz configuration object
-	    liveVizConfig lvConfig(liveVizConfig::pix_color, true);
+	        // Initialize the liveViz library
+	        liveVizInit(lvConfig, pixel, cb0, myOpts);
 
-	    // Initialize the liveViz library
-	    liveVizInit(lvConfig, skitArray, cb0, myOpts);
-
-	    /*double resumeTime = CmiWallTimer() + PAUSE_TIME + PAUSE_TIME;     // Pause for PAUSE_TIME seconds
-	     
-	    CkPrintf("Stalling after setup for %f s\n", PAUSE_TIME + PAUSE_TIME);
-	    int j = 0;
-	    while(CmiWallTimer() < resumeTime) j++;*/
+	        /*double resumeTime = CmiWallTimer() + PAUSE_TIME + PAUSE_TIME;     // Pause for PAUSE_TIME seconds
+	         
+	        CkPrintf("Stalling after setup for %f s\n", PAUSE_TIME + PAUSE_TIME);
+	        int j = 0;
+	        while(CmiWallTimer() < resumeTime) j++;*/
 	
-    }
+        }
     
 
 };
 
-/*array [2D]*/
+
 class PixelChare : public CBase_PixelChare 
 {
     public:
-    PixelChare_SDAG_CODE    
+    PixelChare_SDAG_CODE;    
     typedef struct { byte r, g, b; } rgb;
-    vector<rgb> pixels;
+    vector<rgb> pixelArray;
+    double *tmpBuffer;
     int x, y, w, h;
     int iteration;
 
@@ -94,11 +100,25 @@ class PixelChare : public CBase_PixelChare
         iteration = 0;
         w = width;
         h = height;
+        for(int i = 0; i < width * height; i++)
+        {
+            rgb p;
+            p.r = 255;
+            p.g = 0;
+            p.b = 0;
+            pixelArray.push_back(p);
+        }
+        tmpBuffer =  new double[w*h];
     };
 
     PixelChare(CkMigrateMessage *m)
     {
         __sdag_init();
+    };
+
+    ~PixelChare() 
+    {
+        if(tmpBuffer != NULL) delete [] tmpBuffer;
     };
     
     void doWork()
@@ -108,51 +128,40 @@ class PixelChare : public CBase_PixelChare
         
     }
 
-    float xValue(float x) 
-    {
-        return (x - thisIndex.x/chareDimension) * chareDimension;
-    }
-
-    float yValue(float y) 
-    {
-        return (y - thisIndex.y/chareDimension) * chareDimension;
-    }
-
 
     void liveVizDump(liveVizRequestMsg *m) 
-    {
-        int arrayDim = 256/chareDimension;
-        int xCar, yCar;
-        int Cal;
+    {        
+        rgb* imageBuff = (rgb*)tmpBuffer;
         
-        rgb* imageBuff = new rgb[arrayDim * arrayDim];
-        CkAssert(sizeof(rgb) == 3);
-        
-        for(int i =0;i<arrayDim * arrayDim;i++) 
-        {
-            imageBuff[i].r=0;
-            imageBuff[i].g=0;
-            imageBuff[i].b=0;
-        }
-        
-        for (int index = 0; index < pixels.size();index++) 
-        {
-            xCar =(int) ((float) xValue(pixels[index].x) * (float)arrayDim) ;
-            yCar = (int) ((float) yValue(pixels[index].y) * (float)arrayDim) ;
+        register int imgIndex;
+	    rgb* c;
+	    for (int x = 0; x < w; x++) 
+	    {
+		    for (int y = 0; y < h; y++) 
+		    {
+                imgIndex = (y * w) + x;
+			    c = &(imageBuff[imgIndex]);
+			    c->r = pixelArray[imgIndex].r;
+			    c->g = pixelArray[imgIndex].g;
+			    c->b = pixelArray[imgIndex].b;
+		    }
+	    }
 
-            Cal = ((yCar * arrayDim) + xCar);
-            if(pixels[index].color == 'R')
-                imageBuff[Cal%(arrayDim*arrayDim)].r = 255 ;
-            else if(pixels[index].color == 'G')
-                imageBuff[Cal%(arrayDim*arrayDim)].g = 255;
-            else if(pixels[index].color == 'B')
-                imageBuff[Cal%(arrayDim*arrayDim)].b = 255;
-        }
+        liveVizDeposit(m, thisIndex.x * w, thisIndex.y * h,
+                        w, h, (byte*)(imageBuff), this);
 
-        liveVizDeposit(m, thisIndex.x * arrayDim, thisIndex.y * arrayDim,
-        arrayDim, arrayDim, reinterpret_cast<byte*>(imageBuff),
-        this, max_image_data);
-        delete [] imageBuff;
+
+          /*  for (int index = 0; index < pixelArray.size();index++) 
+            {
+                xCar =(int) ((float) xValue(index%w) * (float)arrayDim) ;
+                yCar = (int) ((float) yValue(index/w) * (float)arrayDim) ;
+
+                Cal = ((yCar * arrayDim) + xCar);
+                imageBuff[index].r = pixelArray[index].r;
+                imageBuff[Cal%(arrayDim*arrayDim)].g = pixelArray[index].g;
+                imageBuff[Cal%(arrayDim*arrayDim)].b = pixelArray[index].b;
+            }*/
+
     }
 
 
