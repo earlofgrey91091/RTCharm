22dab33cbf84cb50d1256af2d0e0c0cf068dc79d
earlofgrey91091 <earlofgrey91091@gmail.com>
2012-11-28 11:26:09 -0600

    common.h

---
+++ src/common.h
@@ -0,0 +1,11 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define ITERATIONS 1000
+#define SHAPES 20
+#define loc(x,y, width)  (x + (y*width))
+#define rnd(x) (x*rand() / RAND_MAX)
+#define BOX_DIM 1000
+
+
+#endif //__COMMON_H__
